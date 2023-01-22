ac8dd5cee88c04dcfeb5beff5d77e5d3048df6f7
David <devolov@gmail.com>
2023-01-22 14:47:47 -0500

    Remove the extra save confirmation

--- src/start_menu.c
+++ src/start_menu.c
@@ -1108,7 +1108,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
