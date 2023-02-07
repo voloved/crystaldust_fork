38ea9d508fc47b643b066b195aaf022f479d65b7
David Volovskiy <devolov@gmail.com>
2023-02-06 20:38:15 -0500

    Fixed mon to switch into text logic...though it likely won't change behavior

--- src/battle_main.c
+++ src/battle_main.c
@@ -5068,8 +5068,6 @@ static void HandleEndTurn_MonFled(void)
 
 static void HandleEndTurn_FinishBattle(void)
 {
-    gBattleMons[B_SIDE_OPPONENT].species = SPECIES_NONE;  // So the "Choose a Pkmn message doesn't include the last mon battled outside of battle."
-    *(gBattleStruct->monToSwitchIntoId + B_SIDE_OPPONENT) = SPECIES_NONE;  // Should already be none at the end of a battle, but better to be explicit
     if (gCurrentActionFuncId == B_ACTION_TRY_FINISH || gCurrentActionFuncId == B_ACTION_FINISHED)
     {
         if (!(gBattleTypeFlags & (BATTLE_TYPE_LINK
