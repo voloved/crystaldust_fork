voloved/pokeemerald_fork

Added Roserade, Ambipom, Mismagius, Honchkrow, and Froslass

src/pokemon.c
@@ -354,11 +354,6 @@ static const u16 sSpeciesToHoennPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_HOENN(OLD_UNOWN_I),
     SPECIES_TO_HOENN(OLD_UNOWN_K),
     SPECIES_TO_HOENN(OLD_UNOWN_N),
-    SPECIES_TO_HOENN(OLD_UNOWN_U),
-    SPECIES_TO_HOENN(OLD_UNOWN_V),
-    SPECIES_TO_HOENN(OLD_UNOWN_W),
-    SPECIES_TO_HOENN(OLD_UNOWN_X),
-    SPECIES_TO_HOENN(OLD_UNOWN_Y),
     SPECIES_TO_HOENN(OLD_UNOWN_Z),
     SPECIES_TO_HOENN(TREECKO),
     SPECIES_TO_HOENN(GROVYLE),
@@ -510,6 +505,11 @@ static const u16 sSpeciesToHoennPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_HOENN(GALLADE),
     SPECIES_TO_HOENN(PROBOPASS),
     SPECIES_TO_HOENN(DUSKNOIR),
+    SPECIES_TO_HOENN(ROSERADE),
+    SPECIES_TO_HOENN(AMBIPOM),
+    SPECIES_TO_HOENN(MISMAGIUS),
+    SPECIES_TO_HOENN(HONCHKROW),
+    SPECIES_TO_HOENN(FROSLASS),
     SPECIES_TO_HOENN(LEAFEON),
     SPECIES_TO_HOENN(GLACEON),
     SPECIES_TO_HOENN(SYLVEON),
@@ -774,11 +774,6 @@ static const u16 sSpeciesToNationalPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_NATIONAL(OLD_UNOWN_I),
     SPECIES_TO_NATIONAL(OLD_UNOWN_K),
     SPECIES_TO_NATIONAL(OLD_UNOWN_N),
-    SPECIES_TO_NATIONAL(OLD_UNOWN_U),
-    SPECIES_TO_NATIONAL(OLD_UNOWN_V),
-    SPECIES_TO_NATIONAL(OLD_UNOWN_W),
-    SPECIES_TO_NATIONAL(OLD_UNOWN_X),
-    SPECIES_TO_NATIONAL(OLD_UNOWN_Y),
     SPECIES_TO_NATIONAL(OLD_UNOWN_Z),
     SPECIES_TO_NATIONAL(TREECKO),
     SPECIES_TO_NATIONAL(GROVYLE),
@@ -930,6 +925,11 @@ static const u16 sSpeciesToNationalPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_NATIONAL(GALLADE),
     SPECIES_TO_NATIONAL(PROBOPASS),
     SPECIES_TO_NATIONAL(DUSKNOIR),
+    SPECIES_TO_NATIONAL(ROSERADE),
+    SPECIES_TO_NATIONAL(AMBIPOM),
+    SPECIES_TO_NATIONAL(MISMAGIUS),
+    SPECIES_TO_NATIONAL(HONCHKROW),
+    SPECIES_TO_NATIONAL(FROSLASS),
     SPECIES_TO_NATIONAL(LEAFEON),
     SPECIES_TO_NATIONAL(GLACEON),
     SPECIES_TO_NATIONAL(SYLVEON),
@@ -1341,6 +1341,11 @@ static const u16 sHoennToNationalOrder[NUM_SPECIES - 1] =
     HOENN_TO_NATIONAL(GALLADE),
     HOENN_TO_NATIONAL(PROBOPASS),
     HOENN_TO_NATIONAL(DUSKNOIR),
+    HOENN_TO_NATIONAL(ROSERADE),
+    HOENN_TO_NATIONAL(AMBIPOM),
+    HOENN_TO_NATIONAL(MISMAGIUS),
+    HOENN_TO_NATIONAL(HONCHKROW),
+    HOENN_TO_NATIONAL(FROSLASS),
     HOENN_TO_NATIONAL(LEAFEON),
     HOENN_TO_NATIONAL(GLACEON),
     HOENN_TO_NATIONAL(SYLVEON),
@@ -1348,11 +1353,6 @@ static const u16 sHoennToNationalOrder[NUM_SPECIES - 1] =
     HOENN_TO_NATIONAL(OLD_UNOWN_I),
     HOENN_TO_NATIONAL(OLD_UNOWN_K),
     HOENN_TO_NATIONAL(OLD_UNOWN_N),
-    HOENN_TO_NATIONAL(OLD_UNOWN_U),
-    HOENN_TO_NATIONAL(OLD_UNOWN_V),
-    HOENN_TO_NATIONAL(OLD_UNOWN_W),
-    HOENN_TO_NATIONAL(OLD_UNOWN_X),
-    HOENN_TO_NATIONAL(OLD_UNOWN_Y),
     HOENN_TO_NATIONAL(OLD_UNOWN_Z),
 };
 
@@ -1810,10 +1810,15 @@ static const u8 sMonFrontAnimIdsTable[NUM_SPECIES - 1] =
     [SPECIES_GALLADE - 1]       = ANIM_SHRINK_GROW,
     [SPECIES_PROBOPASS - 1]     = ANIM_SWING_CONVEX_FAST,
     [SPECIES_DUSKNOIR - 1]      = ANIM_GLOW_BLACK,
+    [SPECIES_ROSERADE - 1]      = ANIM_H_VIBRATE,
+    [SPECIES_AMBIPOM - 1]       = ANIM_BACK_AND_LUNGE,
+    [SPECIES_MISMAGIUS - 1]     = ANIM_H_SLIDE_WOBBLE,
+    [SPECIES_HONCHKROW - 1]     = ANIM_V_SQUISH_AND_BOUNCE_SLOW,
+    [SPECIES_FROSLASS - 1]      = ANIM_V_SLIDE_WOBBLE,
     [SPECIES_LEAFEON - 1]       = ANIM_V_JUMPS_SMALL,
     [SPECIES_GLACEON - 1]       = ANIM_V_STRETCH,
     [SPECIES_SYLVEON - 1]       = ANIM_H_STRETCH,
-    [SPECIES_MISSINGNO - 1]    = ANIM_TWIST,
+    [SPECIES_MISSINGNO - 1]     = ANIM_TWIST,
 };
 
 static const u8 sMonAnimationDelayTable[NUM_SPECIES - 1] =

voloved/crystaldust_fork

src/pokemon.c
@@ -529,6 +529,11 @@ const u16 gSpeciesToJohtoPokedexNum[] = // Assigns all species to the Johto Dex 
     SPECIES_TO_JOHTO(GALLADE),
     SPECIES_TO_JOHTO(PROBOPASS),
     SPECIES_TO_JOHTO(DUSKNOIR),
+    SPECIES_TO_JOHTO(ROSERADE),
+    SPECIES_TO_JOHTO(AMBIPOM),
+    SPECIES_TO_JOHTO(MISMAGIUS),
+    SPECIES_TO_JOHTO(HONCHKROW),
+    SPECIES_TO_JOHTO(FROSLASS),
     SPECIES_TO_JOHTO(LEAFEON),
     SPECIES_TO_JOHTO(GLACEON),
     SPECIES_TO_JOHTO(MISSINGNO),
@@ -962,6 +967,11 @@ const u16 gSpeciesToNationalPokedexNum[] = // Assigns all species to the Nationa
     SPECIES_TO_NATIONAL(GALLADE),
     SPECIES_TO_NATIONAL(PROBOPASS),
     SPECIES_TO_NATIONAL(DUSKNOIR),
+    SPECIES_TO_NATIONAL(ROSERADE),
+    SPECIES_TO_NATIONAL(AMBIPOM),
+    SPECIES_TO_NATIONAL(MISMAGIUS),
+    SPECIES_TO_NATIONAL(HONCHKROW),
+    SPECIES_TO_NATIONAL(FROSLASS),
     SPECIES_TO_NATIONAL(LEAFEON),
     SPECIES_TO_NATIONAL(GLACEON),
     SPECIES_TO_NATIONAL(MISSINGNO),
@@ -1395,6 +1405,11 @@ const u16 gJohtoToNationalOrder[] = // Assigns Johto Dex Pokémon (Using Nationa
     JOHTO_TO_NATIONAL(GALLADE),
     JOHTO_TO_NATIONAL(PROBOPASS),
     JOHTO_TO_NATIONAL(DUSKNOIR),
+    JOHTO_TO_NATIONAL(ROSERADE),
+    JOHTO_TO_NATIONAL(AMBIPOM),
+    JOHTO_TO_NATIONAL(MISMAGIUS),
+    JOHTO_TO_NATIONAL(HONCHKROW),
+    JOHTO_TO_NATIONAL(FROSLASS),
     JOHTO_TO_NATIONAL(LEAFEON),
     JOHTO_TO_NATIONAL(GLACEON),
     JOHTO_TO_NATIONAL(MISSINGNO),
@@ -1854,6 +1869,11 @@ static const u8 sMonFrontAnimIdsTable[] =
     [SPECIES_GALLADE - 1]       = ANIM_SHRINK_GROW,
     [SPECIES_PROBOPASS - 1]     = ANIM_SWING_CONVEX_FAST,
     [SPECIES_DUSKNOIR - 1]      = ANIM_GLOW_BLACK,
+    [SPECIES_ROSERADE - 1]      = ANIM_H_VIBRATE,
+    [SPECIES_AMBIPOM - 1]       = ANIM_BACK_AND_LUNGE,
+    [SPECIES_MISMAGIUS - 1]     = ANIM_H_SLIDE_WOBBLE,
+    [SPECIES_HONCHKROW - 1]     = ANIM_V_SQUISH_AND_BOUNCE_SLOW,
+    [SPECIES_FROSLASS - 1]      = ANIM_V_SLIDE_WOBBLE,
     [SPECIES_LEAFEON - 1]       = ANIM_V_JUMPS_SMALL,
     [SPECIES_GLACEON - 1]       = ANIM_V_STRETCH, 
     [SPECIES_MISSINGNO - 1]    = ANIM_TWIST,   