voloved/pokeemerald_fork

Acrobatic's logic is similar to other moves for damage handling now

src/battle_script_commands.c
@@ -332,6 +332,7 @@ static void Cmd_finishaction(void);
 static void Cmd_finishturn(void);
 static void Cmd_trainerslideout(void);
 static void Cmd_ballthrowend(void);
+static void Cmd_helditemtodamagecalculation(void);
 
 void (* const gBattleScriptingCommandsTable[])(void) =
 {
@@ -584,7 +585,8 @@ void (* const gBattleScriptingCommandsTable[])(void) =
     Cmd_finishaction,                            //0xF6
     Cmd_finishturn,                              //0xF7
     Cmd_trainerslideout,                         //0xF8
-    Cmd_ballthrowend                             //0xF9
+    Cmd_ballthrowend,                            //0xF9
+    Cmd_helditemtodamagecalculation              //0xFA
 };
 
 struct StatFractions
@@ -10655,6 +10657,15 @@ static void Cmd_ballthrowend(void)
     gBallShakesBData.odds = 0;
     gBallShakesBData.ballShakesArray = 0;
     gBallShakesBData.shakes = 0;
+}
+
+static void Cmd_helditemtodamagecalculation(void)
+{
+    gDynamicBasePower = gBattleMoves[gCurrentMove].power;
+    if (gBattleMons[gBattlerAttacker].item == ITEM_NONE)
+        gDynamicBasePower *= 2;
+
+    gBattlescriptCurrInstr++;
 }
 
 bool8 CalcNextShakeFromOdds(u32 odds)

voloved/crystaldust_fork

src/battle_script_commands.c
@@ -331,6 +331,7 @@ static void Cmd_trainerslideout(void);
 static void Cmd_setcaughtbugcatchingcontestmon(void);
 static void Cmd_swapbugcatchingcontestmon(void);
 static void Cmd_ballthrowend(void);
+static void Cmd_helditemtodamagecalculation(void);
 
 void (* const gBattleScriptingCommandsTable[])(void) =
 {
@@ -585,7 +586,8 @@ void (* const gBattleScriptingCommandsTable[])(void) =
     Cmd_trainerslideout,                         //0xF8
     Cmd_setcaughtbugcatchingcontestmon,          //0xF9
     Cmd_swapbugcatchingcontestmon,               //0xFA
-    Cmd_ballthrowend                             //0xFB
+    Cmd_ballthrowend,                            //0xFB
+    Cmd_helditemtodamagecalculation              //0xFC
 };
 
 struct StatFractions
@@ -10406,6 +10408,15 @@ static void Cmd_ballthrowend(void)
     gBallShakesBData.odds = 0;
     gBallShakesBData.ballShakesArray = 0;
     gBallShakesBData.shakes = 0;
+}
+
+static void Cmd_helditemtodamagecalculation(void)
+{
+    gDynamicBasePower = gBattleMoves[gCurrentMove].power;
+    if (gBattleMons[gBattlerAttacker].item == ITEM_NONE)
+        gDynamicBasePower *= 2;
+
+    gBattlescriptCurrInstr++;
 }
 
 bool8 CalcNextShakeFromOdds(u32 odds)