voloved/pokeemerald_fork

Surfing Occurs Without Asking After the First Time Surfing on a Map.

include/constants/flags.h
@@ -22,7 +22,7 @@
 #define FLAG_TEMP_C      (TEMP_FLAGS_START + 0xC)  // Unused Flag
 #define FLAG_TEMP_MEAN_ZIGZAGOON     (TEMP_FLAGS_START + 0xD)  // Used for the Mean Zigzagoon battle; Also used for cheap vitamins after E4
 #define FLAG_TEMP_HIDE_FOLLOWER      (TEMP_FLAGS_START + 0xE)  // When set, follower pokemon won't be spawned
-#define FLAG_TEMP_F      (TEMP_FLAGS_START + 0xF)  // Unused Flag
+#define FLAG_TEMP_SURF_SKIP_INTRO    (TEMP_FLAGS_START + 0xF)  // When set, surf occurs when walking into water without intro
 #define FLAG_TEMP_10     (TEMP_FLAGS_START + 0x10) // Unused Flag
 #define FLAG_TEMP_11     (TEMP_FLAGS_START + 0x11)
 #define FLAG_TEMP_12     (TEMP_FLAGS_START + 0x12)

src/field_player_avatar.c
@@ -30,6 +30,7 @@
 #include "constants/moves.h"
 #include "constants/songs.h"
 #include "constants/trainer_types.h"
+#include "event_scripts.h"
 
 #define NUM_FORCED_MOVEMENTS 18
 #define NUM_ACRO_BIKE_COLLISIONS 5
@@ -768,6 +769,13 @@ u8 CheckForObjectEventCollision(struct ObjectEvent *objectEvent, s16 x, s16 y, u
     u8 collision = GetCollisionAtCoords(objectEvent, x, y, direction);
     if (collision == COLLISION_ELEVATION_MISMATCH && CanStopSurfing(x, y, direction))
         return COLLISION_STOP_SURFING;
+    if (IsPlayerFacingSurfableFishableWater() 
+        && (!TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_SURFING))
+        && GetObjectEventIdByPosition(x, y, 1) == OBJECT_EVENTS_COUNT)
+    {
+        ScriptContext_SetupScript(EventScript_UseSurfNoIntro);
+        return COLLISION_SURF_NO_INTRO;
+    }
 
     if (ShouldJumpLedge(x, y, direction))
     {

voloved/crystaldust_fork

include/constants/flags.h
@@ -1635,6 +1635,7 @@
 #define FLAG_ENABLE_MULTI_CORRIDOR_DOOR         (SPECIAL_FLAGS_START + 0x2)
 #define FLAG_SPECIAL_FLAG_UNUSED_0x4003         (SPECIAL_FLAGS_START + 0x3) // Unused Flag
 #define FLAG_STORING_ITEMS_IN_PYRAMID_BAG       (SPECIAL_FLAGS_START + 0x4)
+#define FLAG_SURF_SKIP_INTRO                    (SPECIAL_FLAGS_START + 0x5)  // When set, surf occurs when walking into water without intro
 // FLAG_SPECIAL_FLAG_0x4005 - 0x407F also exist and are unused
 #define SPECIAL_FLAGS_END                       (SPECIAL_FLAGS_START + 0x7F)
 

src/field_player_avatar.c
@@ -34,6 +34,7 @@
 #include "constants/songs.h"
 #include "constants/trainer_types.h"
 #include "item.h"
+#include "event_scripts.h"
 
 EWRAM_DATA bool8 gRunToggleBtnSet = FALSE;
 
@@ -728,6 +729,13 @@ u8 CheckForObjectEventCollision(struct ObjectEvent *objectEvent, s16 x, s16 y, u
     u8 collision = GetCollisionAtCoords(objectEvent, x, y, direction);
     if (collision == COLLISION_ELEVATION_MISMATCH && CanStopSurfing(x, y, direction))
         return COLLISION_STOP_SURFING;
+    if (IsPlayerFacingSurfableFishableWater() 
+        && (!TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_SURFING))
+        && GetObjectEventIdByXYZ(x, y, 1) == OBJECT_EVENTS_COUNT)
+    {
+        ScriptContext1_SetupScript(EventScript_UseSurfNoIntro);
+        return COLLISION_SURF_NO_INTRO;
+    }
 
     if (ShouldJumpLedge(x, y, direction))
     {