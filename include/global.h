bd365cded12063ed32401e2a7d93c5e40034e200
David Volovskiy <devolov@gmail.com>
2026-09-16 08:15:28 -0400

    Declarations for 2- and 3-argument GetMonData

--- include/global.h
+++ include/global.h
@@ -137,6 +137,13 @@
 #define DIFFICULTY_HARD      1
 #define DIFFICULTY_EASY      2
 
+// This returns the number of arguments passed to it (up to 8).
+#define NARG_8(...) NARG_8_(_, ##__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define NARG_8_(_, a, b, c, d, e, f, g, h, N, ...) N
+
+#define CAT(a, b) CAT_(a, b)
+#define CAT_(a, b) a ## b
+
 struct Coords8
 {
     s8 x;
